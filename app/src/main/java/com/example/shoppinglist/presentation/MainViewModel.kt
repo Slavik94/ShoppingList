c22c4ff98bde2869bc2a0ad14b1e6835f863e492
Slavik94 <v.kovalchuk@sharksw.com>
2022-02-20 21:45:19 +0200

    added Presentation Layer

---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.shoppinglist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.DeleteShopItemUseCase
+import com.example.shoppinglist.domain.EditShopItemUseCase
+import com.example.shoppinglist.domain.GetShopItemListUseCase
+import com.example.shoppinglist.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+    private val repository = ShopListRepositoryImpl
+
+
+
+    private val getShopItemListUseCase = GetShopItemListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopItemListUseCase.getShopItemList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+
+
+}
